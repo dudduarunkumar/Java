e605f2d25909e3815b6dcdbf5ddbd81d5fc750c4
rn-arun <dudduarunkumar@gmail.com>
2023-11-08 22:40:28 +0530

    java

---
+++ Abc.java
@@ -0,0 +1,12 @@
+class Employee{
+    public String firstName="Kohli";
+    public int age=30;
+}
+
+public class Abc{
+    public static void main(String[] args){
+        Employee e1=new Employee();
+        System.out.println(e1);
+        // System.out.println("Arun");
+    }
+}
